b442cb89594bef5548c914ac481d2e34ec9fdd41
Grace Doyle <graceydoyle@gmail.com>
2022-03-04 01:29:35 +0000

    Version 1.0 first draft but not complete

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,23 +1,44 @@
 import { db } from "../models/db.js";
+import { CollectionSpec } from "../models/joi-schemas.js";
+
 
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      const collections = await db.collectionStore.getAllCollections();
+      const loggedInUser = request.auth.credentials;
+      const collections = await db.collectionStore.getUserCollections(loggedInUser._id);
       const viewData = {
-        title: "GeoSurf Dashboard",
-        collections: collections,
+        title: "Surf Collection Dashboard",
+        user: loggedInUser,
+     collections: collections,
       };
       return h.view("dashboard-view", viewData);
     },
   },
 
   addCollection: {
+    validate: {
+      payload: CollectionSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("dashboard-view", { title: "Add Collection error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
+      const loggedInUser = request.auth.credentials;
       const newCollection = {
+        userid: loggedInUser._id,
         title: request.payload.title,
       };
       await db.collectionStore.addCollection(newCollection);
+      return h.redirect("/dashboard");
+    },
+  },
+
+  deleteCollection: {
+    handler: async function (request, h) {
+      const collection = await db.collectionStore.getCollectionById(request.params.id);
+      await db.collectionStore.deleteCollectionById(collection._id);
       return h.redirect("/dashboard");
     },
   },
