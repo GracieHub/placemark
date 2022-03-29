30064cb781994a6ab1e3bdf3ad4c6f6c0b2b97b3
Grace Doyle <graceydoyle@gmail.com>
2022-03-29 04:12:01 +0100

    updated server.js and fixed image bug

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 const result = dotenv.config();
 if (result.error) {
   console.log(result.error.message);
-  process.exit(1);
+//  process.exit(1);
 }
 
 const swaggerOptions = {
@@ -101,7 +101,7 @@ async function init() {
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
-  process.exit(1);
+//  process.exit(1);
 });
 
 init();
